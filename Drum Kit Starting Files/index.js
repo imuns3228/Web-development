fea64ff8f48f93120dfa20f687ec523b34cd9e25
imuns3228 <82765620+imuns3228@users.noreply.github.com>
2022-02-08 15:21:42 +0200

    update var to let / const

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,10 @@
 // Detecting Button Clicked
 
-var length = document.querySelectorAll(".drum").length;
+const length = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var buttonInnerHTML = this.innerHTML;
+    let buttonInnerHTML = this.innerHTML;
     playKey(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
@@ -25,37 +25,37 @@ for (let i = 0; i < length; i++) {
 function playKey(letter){
   switch (letter) {
     case "w":
-      var kick = new Audio("sounds/kick-bass.mp3");
+      let kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
 
     case "a":
-      var snare = new Audio("sounds/snare.mp3");
+      let snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
     case "s":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      let tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "d":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      let tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "j":
-      var tom3 = new Audio("sounds/tom-3.mp3");
+      let tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
     case "k":
-      var tom4 = new Audio("sounds/tom-4.mp3");
+      let tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
     case "l":
-      var crash = new Audio("sounds/crash.mp3");
+      let crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
